1fda453d79553a369636a8fd6ef683493a162d67
Joshua Carroll <carrolljosh1976@gmail.com>
2022-12-17 12:42:51 -0500

    added NavLink, NavLinks, and Header components

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title></title>
+      <title>Josh's News App</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
     </>
