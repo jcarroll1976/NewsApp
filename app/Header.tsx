ebb0587e204fef2e1623349cedf1474a3cca1be0
Joshua Carroll <carrolljosh1976@gmail.com>
2023-04-30 23:41:57 -0400

    Added h1,icon, and subscribe button to header

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import {Bars3Icon} from "@heroicons/react/24/solid";
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header>
+        <div className="grid grid-cols-3 p-10 items-center">
+            <Bars3Icon className="h-8 w-8 cursor-pointer" />
+            <Link href="/" prefetch={false}>
+                <h1>Your Daily News</h1>
+            </Link>
+            <div className="flex items-center justify-end space-x-2">
+                {/*Dark Mode Button*/}
+                <button className="hidden md:inline bg-slate-900 text-white 
+                px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800">Subscribe Now</button>
+            </div>
+        </div>
+    </header>
   )
 }
