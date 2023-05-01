5dc543d01c9958b977d1938f16dc00c1e7e66681
Joshua Carroll <carrolljosh1976@gmail.com>
2023-05-01 19:46:17 -0400

    Finished Header

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Bars3Icon} from "@heroicons/react/24/solid";
 import Link from 'next/link';
+import NavLinks from './NavLinks';
 
 export default function Header() {
   return (
@@ -8,7 +9,7 @@ export default function Header() {
         <div className="grid grid-cols-3 p-10 items-center">
             <Bars3Icon className="h-8 w-8 cursor-pointer" />
             <Link href="/" prefetch={false}>
-                <h1>Your Daily News</h1>
+                <h1 className="font-serif text-4xl">Your <span className="underline decoration-6 decoration-orange-400">Daily</span> News</h1>
             </Link>
             <div className="flex items-center justify-end space-x-2">
                 {/*Dark Mode Button*/}
@@ -16,6 +17,13 @@ export default function Header() {
                 px-4 lg:px-8 py-2 lg:py-4 rounded-full dark:bg-slate-800">Subscribe Now</button>
             </div>
         </div>
+
+        {/*NavLinks*/}
+
+        <NavLinks />
+
+        {/*SearchBox*/}
+
     </header>
   )
 }
---
+++ app/NavLinks.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { categories } from '@/constants';
+
+export default function NavLinks() {
+  return (
+    <div>NavLinks</div>
+  )
+}
